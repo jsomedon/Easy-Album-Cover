2033c0921c4f1f9d13ac0155c4ebd10494e11724
Jay Somedon <jay.somedon@outlook.com>
2019-07-09 05:32:38 +0800

    update build.sh
    Signed-off-by: Jay Somedon <jay.somedon@outlook.com>

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-zip -r -FS ./Easy-Album-Cover.zip * --exclude *.git* *screenshot* *icon.png node_modules test *build* package-lock.json package.json *README* *.zip
+zip -r -FS ./Easy-Album-Cover.zip * --exclude .git* ./img/screenshot.gif ./img/icon.png "./node_modules/*" "./test/*" ./build.sh ./package-lock.json ./package.json ./README.md ./*.zip
